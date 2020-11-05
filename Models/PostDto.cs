670ff639c9a7fba43d06fa3fc06ace4846c729d7
ferhat acar <ingolstadtferhat@hotmail.com>
2020-11-05 21:55:48 +0300

    Post Requestleri için HTTP requestleri oluşturulmuştur

---
+++ Models/PostDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TodoApi.Models
+{
+    public class PostDto
+    {
+        public decimal SQ_ID { get; set; }
+        public Nullable<decimal> RF_USER_ID { get; set; }
+        public string TX_POST { get; set; }
+    }
+}
